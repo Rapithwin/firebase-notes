39835bd457a19d3488713eedb06499810a6f95ac
unknown <khandanamir7@gmail.com>
2025-08-06 15:46:01 +0330

    Firebase auth constants

--- lib/constants/auth_constants.dart
+++ lib/constants/auth_constants.dart
@@ -1,1 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_notes/controllers/auth_controller.dart';
 
+AuthController authController = AuthController.instance;
+final Future<FirebaseApp> firebaseInitialization = Firebase.initializeApp();
+FirebaseAuth auth = FirebaseAuth.instance;
