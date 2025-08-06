d1528cd8f71750c78ea6eb01267d22101a681f8d
unknown <khandanamir7@gmail.com>
2025-08-06 15:46:18 +0330

    onReady method implemented

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,1 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_notes/constants/auth_constants.dart';
+import 'package:get/get.dart';
 
+class AuthController extends GetxController {
+  static AuthController instance = Get.find();
+  late Rx<User?> firebaseUser;
+
+  @override
+  void onReady() {
+    super.onReady();
+
+    firebaseUser = Rx<User?>(auth.currentUser);
+    firebaseUser.bindStream(auth.userChanges());
+
+    ever(firebaseUser, _setInitialScreen);
+  }
+
+  _setInitialScreen(User? user) {
+    if (user != null) {
+      Get.offAll(() => const HomePage());
+    } else {
+      Get.offAll(() => const LoginPage());
+    }
+  }
+}
