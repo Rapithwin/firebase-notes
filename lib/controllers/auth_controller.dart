5d3a16b9ddb144eed49d1adc83e0c0010318660b
unknown <khandanamir7@gmail.com>
2025-08-09 17:33:27 +0330

    Removed unused import

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,7 +1,6 @@
 import 'dart:developer';
 
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_notes/constants/auth_constants.dart';
 import 'package:firebase_notes/pages/home_page.dart';
 import 'package:firebase_notes/pages/login_page.dart';
 import 'package:flutter/material.dart';
