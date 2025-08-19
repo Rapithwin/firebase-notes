7420842e80e9d09452ba786ded3351923f5b8321
unknown <khandanamir7@gmail.com>
2025-08-19 17:02:10 +0330

    ListView and GridView notes ui implemented

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -15,6 +15,16 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final authController = Get.find<AuthController>();
   final themeController = Get.find<ThemeController>();
+
+  Map<String, String> mockNote = {
+    "title 1": "Hello this is title 1 ",
+    "title 2": "Hello this is title 2 and it's supposed to be 2 ",
+    "title 3":
+        "Hello this is title 3 and I'm writing it a little bit longer than the other one.",
+    "title 4":
+        "Hello there and welcome to title 4. I am deliberately writing this longer than usual to see how many lines it will be.",
+  };
+
   @override
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
@@ -54,22 +64,133 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: theme.colorScheme.primaryContainer,
         child: Icon(Icons.edit_outlined),
       ),
-      body: MasonryGridView.count(
-        crossAxisCount: 2,
-        mainAxisSpacing: 4,
-        crossAxisSpacing: 4,
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          return Card(
-            color: theme.colorScheme.surfaceContainerLow,
-            child: Container(
-              height: (index % 5 + 1) * 80,
-              alignment: Alignment.center,
-              child: Text('Note $index'),
-            ),
-          );
-        },
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 26.0),
+        child: ListNotes(mockNote: mockNote, theme: theme),
       ),
+    );
+  }
+}
+
+class ListNotes extends StatelessWidget {
+  const ListNotes({
+    super.key,
+    required this.mockNote,
+    required this.theme,
+  });
+
+  final Map<String, String> mockNote;
+  final ThemeData theme;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: mockNote.length,
+      itemBuilder: (context, index) {
+        return Card(
+          color: theme.colorScheme.surfaceContainerHigh,
+          child: InkWell(
+            onTap: () {},
+            borderRadius: BorderRadius.circular(16),
+            child: Container(
+              // margin: EdgeInsets.only(bottom: 8),s
+              padding: EdgeInsets.only(
+                left: 15,
+                top: 10,
+                right: 15,
+              ),
+              width: Get.size.width,
+              height: 80,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    mockNote.keys.toList()[index],
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Text(
+                    mockNote.values.toList()[index],
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class GridNotes extends StatelessWidget {
+  const GridNotes({
+    super.key,
+    required this.mockNote,
+    required this.theme,
+  });
+
+  final Map<String, String> mockNote;
+  final ThemeData theme;
+
+  @override
+  Widget build(BuildContext context) {
+    return MasonryGridView.count(
+      crossAxisCount: 2,
+
+      itemCount: mockNote.length,
+      itemBuilder: (context, index) {
+        return Card(
+          color: theme.colorScheme.surfaceContainerHigh,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadiusGeometry.circular(16),
+          ),
+          child: InkWell(
+            onTap: () {},
+            borderRadius: BorderRadius.circular(16),
+
+            child: Container(
+              height: mockNote.values.toList()[index].length.toDouble() + 65,
+              constraints: BoxConstraints(
+                minHeight: Get.size.height / 14,
+                maxHeight: Get.size.height / 6.5,
+              ),
+
+              child: Padding(
+                padding: const EdgeInsets.only(left: 12.0, top: 12.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  spacing: 12,
+                  children: [
+                    Text(
+                      mockNote.keys.toList()[index],
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+
+                    Text(
+                      mockNote.values.toList()[index],
+                      maxLines: 4,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
