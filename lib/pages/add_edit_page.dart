12c3045c3c597464eab4b840760ed79282a74993
unknown <khandanamir7@gmail.com>
2025-08-24 20:33:08 +0330

    edit add notes app bar

--- lib/pages/add_edit_page.dart
+++ lib/pages/add_edit_page.dart
@@ -1,12 +1,23 @@
+import 'package:firebase_notes/controllers/theme_controller.dart';
+import 'package:firebase_notes/widgets/custom_widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class AddEditPage extends StatelessWidget {
   const AddEditPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final ThemeController themeController = Get.find<ThemeController>();
     return Scaffold(
-      appBar: AppBar(),
+      appBar: CustomAppBar(
+        themeController: themeController,
+        title: Text(""),
+        titleSpacing: 0,
+        actions: [
+          Icon(Icons.check),
+        ],
+      ),
     );
   }
 }
