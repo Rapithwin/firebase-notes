cb94fa30543b22a81c1a8602340316cab8289293
unknown <khandanamir7@gmail.com>
2025-08-24 19:58:56 +0330

    Settings page ui built

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -12,8 +12,160 @@ class SettingsPage extends StatelessWidget {
     final ThemeController themeController = Get.find<ThemeController>();
     return Scaffold(
       appBar: CustomAppBar(
+        titleSpacing: 0,
         themeController: themeController,
         title: Text("SETTINGS"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          spacing: 5,
+          children: <Widget>[
+            SizedBox(
+              height: 20,
+            ),
+            SettingsTitle(
+              theme: theme,
+              title: "Cloud services",
+            ),
+            SettingsOption(
+              theme: theme,
+              title: "Sync with cloud",
+              icon: Icon(
+                Icons.arrow_forward_ios,
+                size: 18,
+              ),
+              onTap: () {},
+            ),
+            Divider(
+              endIndent: 26,
+              indent: 26,
+            ),
+            SettingsTitle(title: "Style", theme: theme),
+            SettingsOption(
+              theme: theme,
+              title: "Font size",
+              trailing: "Medium",
+              icon: Icon(
+                Icons.arrow_drop_down,
+                size: 25,
+              ),
+              onTap: () {},
+            ),
+            SettingsOption(
+              theme: theme,
+              title: "Layout",
+              trailing: "List view",
+              icon: Icon(
+                Icons.arrow_drop_down,
+                size: 25,
+              ),
+              onTap: () {},
+            ),
+            Divider(
+              endIndent: 26,
+              indent: 26,
+            ),
+            SettingsTitle(
+              theme: theme,
+              title: "Account",
+            ),
+            SettingsOption(
+              theme: theme,
+              title: "Account details",
+              icon: Icon(
+                Icons.arrow_forward_ios,
+                size: 18,
+              ),
+              onTap: () {},
+            ),
+
+            SettingsOption(
+              theme: theme,
+              title: "Sign out",
+              icon: Icon(
+                Icons.arrow_forward_ios,
+                size: 18,
+              ),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SettingsTitle extends StatelessWidget {
+  const SettingsTitle({
+    super.key,
+    required this.title,
+    required this.theme,
+  });
+
+  final ThemeData theme;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 26.0),
+      child: Text(
+        title,
+        style: theme.textTheme.bodyLarge?.copyWith(
+          color: theme.colorScheme.onSurfaceVariant,
+        ),
+      ),
+    );
+  }
+}
+
+class SettingsOption extends StatelessWidget {
+  const SettingsOption({
+    super.key,
+    required this.theme,
+    required this.title,
+    required this.icon,
+    required this.onTap,
+    this.trailing,
+  });
+
+  final ThemeData theme;
+  final String title;
+  final Icon icon;
+  final GestureTapCallback onTap;
+  final String? trailing;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: SizedBox(
+        height: 65,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 26.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                title,
+                style: theme.textTheme.headlineSmall,
+              ),
+              Row(
+                spacing: 16,
+                children: [
+                  Text(
+                    trailing ?? "",
+                    style: theme.textTheme.labelLarge?.copyWith(
+                      color: theme.colorScheme.onSurfaceVariant,
+                    ),
+                  ),
+                  icon,
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
