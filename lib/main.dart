0af1d427122c052c9190d34cadd167b4cbb397cf
unknown <khandanamir7@gmail.com>
2025-08-28 22:15:26 +0330

    Emulator config

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_notes/app.dart';
 import 'package:firebase_notes/constants/auth_constants.dart';
 import 'package:firebase_notes/controllers/auth_controller.dart';
 import 'package:firebase_notes/controllers/theme_controller.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -15,5 +20,15 @@ void main() async {
     ),
   );
   Get.put(ThemeController());
+
+  if (kDebugMode) {
+    log("Using firebase emulator", level: 0);
+    try {
+      FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
+      await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+    } catch (e) {
+      log(e.toString());
+    }
+  }
   runApp(const MainApp());
 }
