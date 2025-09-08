6e912331707b402fd981832e1bcb0c3c7670507e
unknown <khandanamir7@gmail.com>
2025-09-09 00:28:45 +0330

    Added shared preferences to controller to persist data between sessions

--- lib/controllers/style_controller.dart
+++ lib/controllers/style_controller.dart
@@ -1,9 +1,17 @@
 import 'package:firebase_notes/enums/enums.dart';
+import 'package:firebase_notes/extensions/extensions.dart';
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class StyleController extends GetxController {
   Rx<FontSize> fontSize = FontSize.medium.obs;
   Rx<Layout> layout = Layout.list.obs;
+
+  @override
+  void onInit() {
+    loadFontSize();
+    super.onInit();
+  }
 
   double get scaleFactor {
     switch (fontSize.value) {
@@ -18,8 +26,16 @@ class StyleController extends GetxController {
     }
   }
 
-  void changeFontSize(FontSize inputFontSize) {
+  void loadFontSize() async {
+    final prefs = await SharedPreferences.getInstance();
+    final prefsFontSize = prefs.getString("fontSize")!.stringToFontSize;
+    fontSize.value = prefsFontSize;
+  }
+
+  void changeFontSize(FontSize inputFontSize) async {
     fontSize.value = inputFontSize;
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString("fontSize", inputFontSize.fontToString);
   }
 
   void changeLayout(Layout inputLayout) {
